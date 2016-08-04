6342a1c45b1fda0e4aeec89538ce6496ac8f7a6e
Austine Amah <Amani@Air.local>
2016-08-05 00:06:31 +0100

    Added Only user can delete their peegin, and User must be loged in to add a new word

--- app/controllers/peegins_controller.rb
+++ app/controllers/peegins_controller.rb
@@ -1,57 +1,59 @@
 class PeeginsController < ApplicationController
 
   before_action :set_peegin, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
 
   def index
     @peegin = Peegin.all
   end
- 
+
   def show
     @meta_title = @peegin.title
     @meta_description = @peegin.meaning
   end
- 
+
   def new
-    @peegin = Peegin.new
+    @peegin = current_user.peegins.build
   end
- 
+
   def edit
-   
+
   end
- 
+
   def create
-    @peegin = Peegin.new(peegin_params)
- 
+    @peegin = current_user.peegins.build(peegin_params)
+
     if @peegin.save
-      redirect_to @peegin
+      redirect_to @peegin, notice: 'Your Peegin has been added'
     else
       render 'new'
     end
   end
- 
+
   def update
-  
- 
+
+
     if @peegin.update(peegin_params)
       redirect_to @peegin
     else
       render 'edit'
     end
   end
- 
+
   def destroy
-  
+
     @peegin.destroy
- 
+
     redirect_to peegins_path
   end
- 
+
   private
 
     def set_peegin
         @peegin = Peegin.find_by_permalink(params[:id])
       end
+
     def peegin_params
       params.require(:peegin).permit(:title, :meaning, :example, :slug)
     end
