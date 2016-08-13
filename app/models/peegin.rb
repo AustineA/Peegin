950a53fe633136ea9da7c3731358b7e3ac3d33e8
Austine Amah <Amani@Air.local>
2016-08-13 08:56:07 +0100

    Tons of UI Changes for mobile. Added Site helper for mobile,validation for origin and close button to helper

--- app/models/peegin.rb
+++ app/models/peegin.rb
@@ -8,6 +8,8 @@ class Peegin < ActiveRecord::Base
 	validates :title, :length => { :maximum => 50}
 	validates :meaning, :length => { :maximum => 500}
 	validates :example, :length => { :maximum => 150}
+	validates :origin, :length => { :maximum => 50}
+
 
 	def to_param
 		permalink
