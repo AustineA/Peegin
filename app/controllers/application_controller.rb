de8b42b31da1128cd0d831116e88bf5b8fd143e9
Austine Amah <Amani@Air.local>
2016-08-16 11:36:25 +0100

    Redirect back to current page after sign, and clear url when hit the back button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,17 @@ class ApplicationController < ActionController::Base
 
   before_filter :configure_devise_permitted_parameters, if: :devise_controller?
 
+  after_filter :store_location
+
+      def store_location
+        # store last url as long as it isn't a /users path
+        session[:previous_url] = request.fullpath unless request.fullpath =~ /\/users/
+      end
+
+      def after_sign_in_path_for(resource)
+        session[:previous_url] || root_path
+      end
+
     protected
 
     def configure_devise_permitted_parameters
