c8b954b14f0bd03e6e4779250ee42f4219e93a95
Austine Amah <austine.amah@gmail.com>
2021-07-16 21:56:59 +0100

    Fix mimemagic

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,46 +2,38 @@ Rails.application.configure do
   config.webpacker.check_yarn_integrity = false
   config.cache_classes = true
   config.eager_load = true
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
   config.assets.js_compressor = :uglifier
   config.assets.compile = false
   config.active_storage.service = :local
   config.log_level = :debug
-  config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
   config.action_mailer.perform_caching = false
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
   if ENV["RAILS_LOG_TO_STDOUT"].present?
-    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
   config.active_record.dump_schema_after_migration = false
-
-
-
-
-
 
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'no-reply@lunabase.xyz'}
-
-
-
+  config.action_mailer.default_options = { from: "no-reply@lunabase.xyz" }
 
   config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
   config.action_mailer.smtp_settings = {
-    :address => 'in-v3.mailjet.com',
+    :address => "in-v3.mailjet.com",
     :port => 2525,
-    :user_name => 'b188e2b721f01aed40d7646212e22102',
+    :user_name => "b188e2b721f01aed40d7646212e22102",
     :password => Rails.application.credentials.sendjet,
-    :authentication => 'login',
-    :enable_starttls_auto => true
+    :authentication => "login",
+    :enable_starttls_auto => true,
   }
 end
