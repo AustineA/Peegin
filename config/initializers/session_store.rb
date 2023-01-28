3fd48a9da38b75f0576f5b99b3c1db23042f3f1a
Austine <austine.amah@gmail.com>
2023-01-28 08:41:26 +0100

    Set cookie to all domains

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_peegin_eng_dict_session'
+Rails.application.config.session_store :cookie_store, key: '_peegin_eng_dict_session', domain: :all, serializer: :json
+
+# Rails.application.config.action_dispatch.session_store
